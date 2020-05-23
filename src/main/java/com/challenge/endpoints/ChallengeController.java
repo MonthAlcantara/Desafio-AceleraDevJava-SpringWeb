5f93f2b15f5e7f39eaf5fe1b6bd2589e842cd7e2
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 17:42:05 -0300

    Criada lógica do ChallengeController

--- src/main/java/com/challenge/endpoints/ChallengeController.java
+++ src/main/java/com/challenge/endpoints/ChallengeController.java
@@ -1,23 +1,24 @@
 package com.challenge.endpoints;
 
-import com.challenge.entity.Challenge;
-import com.challenge.repository.ChallengeRepository;
-import org.springframework.http.HttpStatus;
+import com.challenge.service.impl.ChallengeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
-import java.util.List;
 
 @RestController
 @RequestMapping("/challenge")
 public class ChallengeController {
-    private ChallengeRepository repository;
+    @Autowired
+    private ChallengeService challengeService;
 
-    @GetMapping("/accelerationId/userId")
-    public ResponseEntity<List<Challenge>> findByAccelerationIdAndUserId(@PathParam("accelerationId") Long accelerationId, @PathParam("userId") Long userId) {
-        return new ResponseEntity<List<Challenge>>(repository.findByAccelerationIdAndUserId(accelerationId, userId),HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<?> findByAccelerationIdAndUserId(@RequestParam(value = "accelerationId") Long accelerationId,
+                                                           @RequestParam(value = "userId") Long userId) {
+        return ResponseEntity.ok((this.challengeService.findByAccelerationIdAndUserId(accelerationId, userId)));
+
+
     }
 }
