3042410992bd206bd6050bc8311379087ba2a5a2
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 17:03:38 -0300

    Criada lógica do SubmissionController

--- src/main/java/com/challenge/endpoints/SubmissionController.java
+++ src/main/java/com/challenge/endpoints/SubmissionController.java
@@ -1,22 +1,29 @@
 package com.challenge.endpoints;
 
 import com.challenge.entity.Submission;
+import com.challenge.mappers.SubmissionMapper;
 import com.challenge.service.impl.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/submission")
 public class SubmissionController {
 
     @Autowired
     private SubmissionService submissionService;
+    @Autowired
+    private SubmissionMapper submissionMapper;
 
-    @RequestMapping("/{challengeId}/{accelerationId}")
-    public List<Submission> findByChallengeIdAndAccelerationId(@PathVariable("challengeId") Long challengeId, @PathVariable("accelerationId") Long accelerationId) {
-        return this.submissionService.findByChallengeIdAndAccelerationId(challengeId, accelerationId);
+    @GetMapping("/{challengeId}/{accelerationId}")
+    public ResponseEntity<?> findByChallengeIdAndAccelerationId(@PathVariable("challengeId") Long challengeId,
+                                                             @PathVariable("accelerationId") Long accelerationId) {
+        return ResponseEntity.ok(submissionMapper.map(submissionService.findByChallengeIdAndAccelerationId(challengeId, accelerationId)));
     }
 }
