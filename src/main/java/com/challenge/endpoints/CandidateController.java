69030700ff3a94bd944e152e169a88e91528a0d1
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 16:56:33 -0300

    Criada lógica do CandidateController

--- src/main/java/com/challenge/endpoints/CandidateController.java
+++ src/main/java/com/challenge/endpoints/CandidateController.java
@@ -1,12 +1,13 @@
 package com.challenge.endpoints;
 
 import com.challenge.entity.Candidate;
-import com.challenge.repository.CandidateRepository;
+import com.challenge.mappers.CandidateMapper;
 import com.challenge.service.impl.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Collections;
 import java.util.Optional;
 
 @RestController
@@ -14,20 +15,27 @@ import java.util.Optional;
 public class CandidateController {
     @Autowired
     private CandidateService candidateService;
+    @Autowired
+    private CandidateMapper candidateMapper;
 
     @GetMapping("/{userId}/{companyId}/{accelerationId}")
-    public Optional<Candidate> findById(@PathVariable("UserId")Long userId, @PathVariable("companyId")Long companyId, @PathVariable("accelerationId")Long accelerationId) {
-        return this.candidateService.findById(userId, companyId, accelerationId);
+    public ResponseEntity<?> findCandidateByUserIdAndCompanyIdAndAccelerationId(@PathVariable("UserId") Long userId,
+                                      @PathVariable("companyId") Long companyId,
+                                      @PathVariable("accelerationId") Long accelerationId) {
+        Optional<Candidate> candidateOptional = candidateService.findById(userId, companyId, accelerationId);
+        return ResponseEntity.ok(candidateMapper.map(candidateOptional.orElse(new Candidate())));
     }
 
-    @GetMapping("/{id}")
-    public List<Candidate> findByCompanyId(@PathVariable("id") Long companyId) {
-        return this.candidateService.findByCompanyId(companyId);
-    }
 
     @GetMapping
-    public List<Candidate> findByAccelerationId(@RequestParam("id") Long accelerationId) {
-        return this.candidateService.findByAccelerationId(accelerationId);
+    public ResponseEntity<?> findByCompanyIdOrAccelerationId(@RequestParam(value = "companyId", required = false) Optional<Long> companyId,
+                                                             @RequestParam(value = "accelerationId", required = false) Optional<Long>  accelerationId) {
+     if(companyId.isPresent()){
+        return ResponseEntity.ok(candidateMapper.map(candidateService.findByCompanyId(companyId.get())));
+        }else if(accelerationId.isPresent()){
+        return ResponseEntity.ok(candidateMapper.map(candidateService.findByAccelerationId(accelerationId.get())));
+        }else
+            return ResponseEntity.ok(Collections.EMPTY_LIST);
     }
 }
 
