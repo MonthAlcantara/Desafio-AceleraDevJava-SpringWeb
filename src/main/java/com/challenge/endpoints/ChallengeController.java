6ce53bd831a014741cefeca3e0bcfecabb33d66c
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 16:16:01 -0300

    Criada lógica do UserController

--- src/main/java/com/challenge/endpoints/ChallengeController.java
+++ src/main/java/com/challenge/endpoints/ChallengeController.java
@@ -2,6 +2,8 @@ package com.challenge.endpoints;
 
 import com.challenge.entity.Challenge;
 import com.challenge.repository.ChallengeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ public class ChallengeController {
     private ChallengeRepository repository;
 
     @GetMapping("/accelerationId/userId")
-    public List<Challenge> findByAccelerationIdAndUserId(@PathParam("accelerationId") Long accelerationId,@PathParam("userId") Long userId) {
-        return repository.findByAccelerationIdAndUserId(accelerationId, userId);
+    public ResponseEntity<List<Challenge>> findByAccelerationIdAndUserId(@PathParam("accelerationId") Long accelerationId, @PathParam("userId") Long userId) {
+        return new ResponseEntity<List<Challenge>>(repository.findByAccelerationIdAndUserId(accelerationId, userId),HttpStatus.OK);
     }
 }
--- src/main/java/com/challenge/endpoints/UserController.java
+++ src/main/java/com/challenge/endpoints/UserController.java
@@ -5,8 +5,11 @@ import com.challenge.repository.UserRepository;
 import com.challenge.service.impl.UserService;
 import com.challenge.service.interfaces.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,19 +20,22 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public Optional<User> findById(@PathVariable("id") Long userId)
-    {
-        return this.userService.findById(userId);
-    }
-
-    @GetMapping("/{name}")
-    public List<User> findByAccelerationName(@PathVariable("name") String name) {
-        return this.userService.findByAccelerationName(name);
+    public ResponseEntity<?> findById(@PathVariable("id") Long userId) {
+        Optional<User> userOptional = this.userService.findById(userId);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping
-    public List<User> findByCompanyId(@RequestParam("id") Long companyId) {
-        return this.userService.findByCompanyId(companyId);
+    public ResponseEntity<?> findByAccelerationNameOrCompanyId(@RequestParam(value = "accelerationName", required = false) Optional<String> accelerationName,
+                                                               @RequestParam(value = "companyId", required = false) Optional<Long> companyId) {
+        if (accelerationName.isPresent()) {
+            return ResponseEntity.ok(this.userService.findByAccelerationName(accelerationName.get()));
+        } else if (companyId.isPresent()) {
+          return ResponseEntity.ok(this.userService.findByCompanyId(companyId.get()));
+        }
+        return ResponseEntity.ok(Collections.EMPTY_LIST);
     }
-
 }
