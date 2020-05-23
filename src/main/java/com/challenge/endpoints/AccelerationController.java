441251229a1435b043e90843ea5ef8fa3c7f3f9d
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 17:12:31 -0300

    Criada lógica do AccelerationController

--- src/main/java/com/challenge/endpoints/AccelerationController.java
+++ src/main/java/com/challenge/endpoints/AccelerationController.java
@@ -3,8 +3,10 @@ package com.challenge.endpoints;
 import com.challenge.entity.Acceleration;
 import com.challenge.service.impl.AccelerationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,12 +17,16 @@ public class AccelerationController {
     private AccelerationService accelerationService;
 
     @GetMapping("/{id}")
-    public Optional<Acceleration> findById(@PathVariable("id") Long id) {
-        return this.accelerationService.findById(id);
+    public ResponseEntity<?> findById(@PathVariable("id") Long id) {
+        Optional<Acceleration> accelerationOptional = this.accelerationService.findById(id);
+           if(accelerationOptional.isPresent()) {
+               return ResponseEntity.ok(accelerationOptional.get());
+           }else
+               return  ResponseEntity.notFound().build();
     }
 
     @GetMapping
-    public List<Acceleration> findByCompanyId(@RequestParam("id") Long companyId) {
-        return this.accelerationService.findByCompanyId(companyId);
+    public ResponseEntity<?> findByCompanyId(@RequestParam(value = "id") Long companyId) {
+        return ResponseEntity.ok(this.accelerationService.findByCompanyId(companyId));
     }
 }
