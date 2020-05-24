094a51889f6c3bd076dea93311f4d8c6f2f6e715
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 22:34:03 -0300

    Realizado ajustes na classe CompanyController

--- src/main/java/com/challenge/endpoints/CompanyController.java
+++ src/main/java/com/challenge/endpoints/CompanyController.java
@@ -1,7 +1,6 @@
 package com.challenge.endpoints;
 
 import com.challenge.entity.Company;
-import com.challenge.repository.CompanyRepository;
 import com.challenge.service.impl.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,27 +13,30 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
+
     @Autowired
     private CompanyService companyService;
 
+    @GetMapping
+    public ResponseEntity<List<Company>> findAllCompanyByAccelerationIdOrUserId(
+            @RequestParam(name = "accelerationId", required = false) Optional<Long> accelerationId,
+            @RequestParam(name = "userId", required = false) Optional<Long> userId) {
+        if (accelerationId.isPresent()) {
+            return ResponseEntity.ok(companyService.findByAccelerationId(accelerationId.get()));
+        } else if (userId.isPresent()) {
+            return ResponseEntity.ok(companyService.findByUserId(userId.get()));
+        } else {
+            return ResponseEntity.ok(Collections.EMPTY_LIST);
+        }
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Company> findById(@PathVariable("id") Long id) {
-        Optional<Company> companyOptional = this.companyService.findById(id);
+    public ResponseEntity<Company> findCompanyById(@PathVariable("id") Long id) {
+        Optional<Company> companyOptional = companyService.findById(id);
         if (companyOptional.isPresent()) {
             return ResponseEntity.ok(companyOptional.get());
         }
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping
-    public ResponseEntity<List<Company>> findByAccelerationIdOrUserId(@RequestParam(name = "accelerationId", required = false) Optional<Long> accelerationId,
-                                                                      @RequestParam(name = "userId", required = false) Optional<Long> userId) {
-        if (accelerationId.isPresent()) {
-            return ResponseEntity.ok(this.companyService.findByAccelerationId(accelerationId.get()));
-
-        } else if (userId.isPresent()) {
-            return ResponseEntity.ok(this.companyService.findByAccelerationId(userId.get()));
-        } else
-            return ResponseEntity.ok(Collections.EMPTY_LIST);
-    }
 }
