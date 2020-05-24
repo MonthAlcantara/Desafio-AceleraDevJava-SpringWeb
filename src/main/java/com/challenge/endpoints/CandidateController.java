9007dc1ab0379a1382e21f597f55d006acd8928d
Montival Junior <montival_junior@yahoo.com.br>
2020-05-23 22:12:19 -0300

    Realizado ajustes nas classes controllers

--- src/main/java/com/challenge/endpoints/CandidateController.java
+++ src/main/java/com/challenge/endpoints/CandidateController.java
@@ -8,36 +8,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
     @Autowired
     private CandidateService candidateService;
     @Autowired
     private CandidateMapper candidateMapper;
 
-    @GetMapping("/{userId}/{companyId}/{accelerationId}")
-    public ResponseEntity<CandidateDTO> findCandidateByUserIdAndCompanyIdAndAccelerationId(@PathVariable("UserId") Long userId,
-                                                                                           @PathVariable("companyId") Long companyId,
-                                                                                           @PathVariable("accelerationId") Long accelerationId) {
-        Optional<Candidate> candidateOptional = candidateService.findById(userId, companyId, accelerationId);
-        return ResponseEntity.ok(candidateMapper.map(candidateOptional.orElse(new Candidate())));
+    @GetMapping
+    public ResponseEntity<List<CandidateDTO>> findAllCandidateByCompanyId(@RequestParam(name = "companyId") Long id) {
+        return ResponseEntity.ok(candidateMapper.map(candidateService.findByCompanyId(id)));
     }
 
-
-    @GetMapping
-    public ResponseEntity<List<CandidateDTO>> findByCompanyIdOrAccelerationId(@RequestParam(name = "companyId", required = false) Optional<Long> companyId,
-                                                                              @RequestParam(name = "accelerationId", required = false) Optional<Long> accelerationId) {
-        if (companyId.isPresent()) {
-            return ResponseEntity.ok(candidateMapper.map(candidateService.findByCompanyId(companyId.get())));
-        } else if (accelerationId.isPresent()) {
-            return ResponseEntity.ok(candidateMapper.map(candidateService.findByAccelerationId(accelerationId.get())));
-        } else
-            return ResponseEntity.ok(Collections.emptyList());
+    @GetMapping("/{userId}/{companyId}/{accelerationId}")
+    public ResponseEntity<CandidateDTO> findCandidateByUserIdAndCompanyIdAndAccelerationId(
+            @PathVariable("userId") Long userId,
+            @PathVariable("companyId") Long companyId,
+            @PathVariable("accelerationId") Long accelerationId) {
+        Optional<Candidate> candidateOptional = candidateService.findById(userId, companyId, accelerationId);
+        return ResponseEntity.ok(candidateMapper.map(candidateOptional.orElse(new Candidate())));
     }
 }
 
